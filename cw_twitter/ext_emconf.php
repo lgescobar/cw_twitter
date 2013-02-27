c96880e6d048c8f184fcfafe72485dfa4837807b
Arjan de Pooter <arjan@cmsworks.nl>
2013-02-27 15:14:57 +0100

    Updated emconf

--- cw_twitter/ext_emconf.php
+++ cw_twitter/ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "cw_twitter".
  *
- * Auto generated 27-02-2013 14:57
+ * Auto generated 27-02-2013 15:12
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Twitter feed',
-	'description' => '',
+	'description' => 'Show tweets from user or search query on frontend. Compatible with the new Twitter API (1.1).',
 	'category' => 'plugin',
 	'author' => 'Arjan de Pooter',
 	'author_email' => 'arjan@cmsworks.nl',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:14:{s:21:"ext_conf_template.txt";s:4:"d20c";s:12:"ext_icon.gif";s:4:"e7c7";s:17:"ext_localconf.php";s:4:"a36e";s:14:"ext_tables.php";s:4:"f862";s:14:"ext_tables.sql";s:4:"6701";s:25:"Classes/Contrib/OAuth.php";s:4:"2f29";s:38:"Classes/Controller/TweetController.php";s:4:"84c4";s:27:"Classes/Utility/Twitter.php";s:4:"334f";s:39:"Configuration/FlexForms/TwitterFeed.xml";s:4:"3c3c";s:38:"Configuration/TypoScript/constants.txt";s:4:"69bd";s:34:"Configuration/TypoScript/setup.txt";s:4:"f384";s:40:"Resources/Private/Language/locallang.xml";s:4:"0011";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"41e4";s:43:"Resources/Private/Templates/Tweet/List.html";s:4:"a8e7";}',
+	'_md5_values_when_last_written' => 'a:14:{s:21:"ext_conf_template.txt";s:4:"d20c";s:12:"ext_icon.gif";s:4:"e7c7";s:17:"ext_localconf.php";s:4:"a36e";s:14:"ext_tables.php";s:4:"f862";s:14:"ext_tables.sql";s:4:"6701";s:25:"Classes/Contrib/OAuth.php";s:4:"2f29";s:38:"Classes/Controller/TweetController.php";s:4:"84c4";s:27:"Classes/Utility/Twitter.php";s:4:"bb85";s:39:"Configuration/FlexForms/TwitterFeed.xml";s:4:"9cdc";s:38:"Configuration/TypoScript/constants.txt";s:4:"69bd";s:34:"Configuration/TypoScript/setup.txt";s:4:"9468";s:40:"Resources/Private/Language/locallang.xml";s:4:"cbb5";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"41e4";s:43:"Resources/Private/Templates/Tweet/List.html";s:4:"613e";}',
 );
 
 ?>
