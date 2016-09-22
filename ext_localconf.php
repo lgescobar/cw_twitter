bd4bbc454647b2aab782c31779262a324f295c63
Guillaume GERMAIN <guillaume@germain.bzh>
2016-09-22 16:30:32 +0200

    Cache configuration for TYPO3 4.6 deleted (depends to TYPO3 6.2+)

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,20 +9,6 @@ if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cwtwitt
 if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cwtwitter_queries']['frontend'])) {
     $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cwtwitter_queries']['frontend'] = '\\TYPO3\\CMS\Core\\Cache\\Frontend\\VariableFrontend';
 }
-if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < '4006000') {
-    if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cwtwitter_queries']['backend'])) {
-        $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cwtwitter_queries']['backend'] = 't3lib_cache_backend_DbBackend';
-    }
-    if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cwtwitter_queries']['options'])) {
-        $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cwtwitter_queries']['options'] = array();
-    }
-    if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cwtwitter_queries']['options']['cacheTable'])) {
-        $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cwtwitter_queries']['options']['cacheTable'] = 'tx_cwtwitter_queries';
-    }
-    if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cwtwitter_queries']['options']['tagsTable'])) {
-        $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cwtwitter_queries']['options']['tagsTable'] = 'tx_cwtwitter_queries_tags';
-    }
-}
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
 	$_EXTKEY,
