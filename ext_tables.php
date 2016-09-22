c9aad53391206e64cc74b7d84cf53c9005ea3b07
Guillaume GERMAIN <guillaume@germain.bzh>
2016-09-22 16:31:16 +0200

    In plugin configuration, hide the field "pages" and show the field "layout" for templating ?!

--- ext_tables.php
+++ ext_tables.php
@@ -9,7 +9,7 @@ if (!defined('TYPO3_MODE')) {
 	'Twitter feed'
 );
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['cwtwitter_pi1'] = 'layout,select_key,recursive';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['cwtwitter_pi1'] = 'select_key,recursive,pages';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['cwtwitter_pi1'] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('cwtwitter_pi1', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/TwitterFeed.xml');
 
