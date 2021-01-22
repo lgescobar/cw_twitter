ac121ec7d347356dade5c3b0e250fd47d6bf9902
Michael Wagner <michael.wagner@dmk-ebusiness.com>
2021-01-22 10:45:42 +0100

    Make installable for typo3 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = [
 
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
+            'typo3' => '8.7.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
